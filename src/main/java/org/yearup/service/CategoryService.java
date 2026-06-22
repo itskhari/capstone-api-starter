284931f2c020b2477a6428a09f4acb7e226e2728
itkhari <mrkharij1@gmail.com>
2026-06-22 13:20:33 -0400

    updated categories controller and service layer, also linked sql database

--- src/main/java/org/yearup/service/CategoryService.java
+++ src/main/java/org/yearup/service/CategoryService.java
@@ -19,29 +19,35 @@ public class CategoryService
     public List<Category> getAllCategories()
     {
         // get all categories
-        return null;
+        return categoryRepository.findAll();
     }
 
     public Category getById(int categoryId)
     {
         // get category by id
-        return null;
+        return categoryRepository.findById(categoryId).orElseThrow(()
+        -> new RuntimeException("Category not found"));
     }
 
     public Category create(Category category)
     {
         // create a new category
-        return null;
+        return categoryRepository.save(category);
     }
 
     public Category update(int categoryId, Category category)
     {
         // update category and return the updated category
-        return null;
+        Category existing = categoryRepository.findById(categoryId)
+                .orElseThrow(() -> new RuntimeException("Category not found"));
+        existing.setName(category.getName());
+
+        return categoryRepository.save(existing);
     }
 
     public void delete(int categoryId)
     {
         // delete category
+        categoryRepository.deleteById(categoryId);
     }
 }
